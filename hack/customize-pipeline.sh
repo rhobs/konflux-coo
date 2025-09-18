3b34ae09fa99166231077a50bf0ebe9e6c8035b9
Hongyan Li <hongyli@redhat.com>
2025-09-18 12:04:49 +0800

    Customize 1.3 piplines and delete 1.2 pipelines

--- hack/customize-pipeline.sh
+++ hack/customize-pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run like so:
 # ./hack/customize-pipeline.sh <pipeline file to customize> [more files]
-branch="release-1.2"
+branch="release-1.3"
 for file in "$@"
 do
     echo "Processing file $file"
