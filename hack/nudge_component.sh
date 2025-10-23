d04eed5ca74c088f3a1424a36be18d1a109d0c06
Hongyan Li <hongyli@redhat.com>
2025-10-23 15:50:58 +0800

    Update bundle template file also nudge perses

--- hack/nudge_component.sh
+++ hack/nudge_component.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # pass the component image ref you want to be nudged as the only argument
 # Can be used like so:
-# kubectl get components -o json | jq -r '.items[] | select(.spec.application == "cluster-observability-operator-1-1") | select(.status.lastPromotedImage != null).status.lastPromotedImage'  | xargs -L1 hack/nudge_component.sh
-
-image="${1:0:(-64)}"
-new_sha="${1:(-64)}"
+# kubectl get components -o json | jq -r '.items[] | select(.spec.application == "cluster-observability-operator-1-3") | select(.status.lastPromotedImage != null).status.lastPromotedImage'  | xargs -L1 hack/nudge_component.sh
+set -x
+echo $1
+image="${1%@sha256:*}"
+new_sha="${1#*@sha256:}"
 echo "Nudging $image" "to" "$new_sha"
-sed -i "s|${image}.*|${image}${new_sha}\"|" bundle-patches/render_templates
+#on MACOS
+sed -i '' "s|${image}@sha256:.*|${image}@sha256:${new_sha}\"|" bundle-patches/render_templates
+#on linux 
+#sed -i "s|${image}@sha256:.*|${image}@sha256:${new_sha}\"|" bundle-patches/render_templates
