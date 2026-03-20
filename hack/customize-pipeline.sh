9909b04f78923c58c08145d4961b4d5f51f9e57d
Jan Fajerski <jfajersk@redhat.com>
2026-03-20 09:49:52 +0100

    customize-pipeline: add pr trigger for merge queue
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

--- hack/customize-pipeline.sh
+++ hack/customize-pipeline.sh
@@ -75,7 +75,8 @@ do
     dockerfile=$(yq '.spec.params[] | select(.name == "dockerfile").value' "$file")
     src="$(grep COPY "$dockerfile" | head -n1 | awk '{print $2}'| cut -d'/' -f1)"
     if [[ "$component" == *"bundle"* ]]; then
-        export trigger="event == \"$action\" && target_branch == \"$branch\" &&
+        export trigger="((event == \"$action\" && target_branch == \"$branch\") ||
+        (event == \"push\" && target_branch.startsWith(\"gh-readonly-queue/main/\"))) &&
         (\".tekton/$component-pull-request.yaml\".pathChanged() ||
         \".tekton/$component-push.yaml\".pathChanged() ||
         \"$dockerfile\".pathChanged() ||
@@ -83,10 +84,11 @@ do
         \"observability-operator/bundle/***\".pathChanged())"
         if [[ $action == "push" ]]; then
             yq -i '.metadata.annotations += {"build.appstudio.openshift.io/build-nudge-files": "hack/update-catalog.sh"}' "$file"
-            yq -i '.spec.params += [{"name": "build-args", "value": ["REGISTRY=registry.redhat.io"]}]' "$file"
+            yq -i 'with(.spec.params; select(all_c(.name != "build-args")) | . += [{"name": "build-args", "value": ["REGISTRY=registry.redhat.io"]}])' "$file"
         fi
     else
-        export trigger="event == \"$action\" && target_branch == \"$branch\" &&
+        export trigger="((event == \"$action\" && target_branch == \"$branch\") ||
+        (event == \"push\" && target_branch.startsWith(\"gh-readonly-queue/main/\"))) &&
         (\".tekton/$component-pull-request.yaml\".pathChanged() ||
         \".tekton/$component-push.yaml\".pathChanged() ||
         \"$dockerfile\".pathChanged() ||
@@ -103,11 +105,14 @@ do
     yq -i '.metadata.annotations += {"pipelinesascode.tekton.dev/on-cel-expression": strenv(trigger)}' "$file"
     yq -i '(.spec.params[] | select(.name == "build-platforms").value | select(length == 1)) += ["linux/arm64","linux/ppc64le","linux/s390x"]' "$file"
     yq -i 'with(.spec.params; select(all_c(.name != "build-source-image")) | . += [{"name": "build-source-image", "value": "true"}])' "$file"
-    yq -i 'with(.spec.params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "value": "true"}])' "$file"
-    yq -i 'with(.spec.pipelineSpec.tasks[] | select(.name == "init").params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "value": "$(params.enable-cache-proxy)"}])' "$file"
-    yq -i 'with(.spec.pipelineSpec.params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "default": "false", "description": "Enable cache proxy configuration", "type": "string"}])' "$file"
+    # yq -i 'with(.spec.params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "value": "true"}])' "$file"
+    # yq -i 'with(.spec.pipelineSpec.tasks[] | select(.name == "init").params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "value": "$(params.enable-cache-proxy)"}])' "$file"
+    # yq -i 'with(.spec.pipelineSpec.params; select(all_c(.name != "enable-cache-proxy")) | . += [{"name": "enable-cache-proxy", "default": "false", "description": "Enable cache proxy configuration", "type": "string"}])' "$file"
     value="build-pipeline-$component" yq -i '.spec.taskRunTemplate += {"serviceAccountName": strenv(value)}' "$file"
     # yq -i 'with(.spec.params; select(all_c(.name != "hermetic")) | . += [{"name": "hermetic", "value": "true"}])' "$file"
     # export gomod_prefetch="$src"
     # yq -i 'with(.spec.params; select(all_c(.name != "prefetch-input")) | . += [{"name": "prefetch-input", "value": "[{\"type\": \"gomod\", \"path\": \"./\(strenv(gomod_prefetch))\"}]"}])' "$file"
+    if [[ $action == "pull-request" ]]; then
+        yq -i '.metadata.labels+= {"build.appstudio.openshift.io/auto-release": "false"}' "$file"
+    fi
 done
